c2fcd9a07da31fbc599e00c6ef3ba6bb958846df
Johanna Schmitz <jschmitz@student.42.fr>
2024-07-02 21:50:39 +0200

    debut

---
+++ libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschmitz <jschmitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 19:39:06 by jschmitz          #+#    #+#             */
+/*   Updated: 2024/07/02 20:31:04 by jschmitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...)
+
+
+#endif
---
+++ printf-original-tester.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf-original-tester.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschmitz <jschmitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 16:13:39 by jschmitz          #+#    #+#             */
+/*   Updated: 2024/07/01 16:19:24 by jschmitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("test normal string");
+//	printf("test with %rercentage");
+	printf("test double %%percentage");
+	return 0;
+}
